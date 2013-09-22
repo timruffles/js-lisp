e08d54493c64d33c8a639f57f28ebe656d4753dc
Tim Ruffles <timruffles@googlemail.com>
2013-09-22 22:27:52 +0100

    J++ is alive

--- interpreter.js
+++ interpreter.js
@@ -1,5 +1,7 @@
 var eval;
 
+var env;
+
 function first(xs) {
   return xs[0]
 }
@@ -12,6 +14,12 @@ function spread(xs,fn) {
 function tap(x,fn) {
   fn(x); return x;
 }
+function ptap(x) {
+  pp(x); return x;
+}
+function pp() {
+  console.log.apply(console,arguments);
+}
 function find(xs,fn) {
   var value;
   xs.some(function(x) {
@@ -21,6 +29,17 @@ function find(xs,fn) {
   })
   return value;
 }
+function zip(xs,ys) {
+  return xs.map(function(x,index) {
+    return [x,ys[index]]
+  });
+}
+function toMap(pairs) {
+  return pairs.reduce(function(map,pair) {
+    map[pair[0]] = pair[1];
+    return map;
+  },{});
+}
 
 function isTrue(expression) {
   return expression !== false
@@ -29,22 +48,24 @@ function isList(expression) {
   return expression instanceof Array;
 }
 function isSymbol(str) {
-  return typeof str == "string" && str[0] == ":";
+  return typeof str == "string";
 }
 
 var anIf = {
+  id: "if",
   is: function(x) {
-    return isList(x) && first(x) == "if"
+    return tagged("if",x);
   },
   eval: function(ifPhrase) {
     return spread(ifPhrase,function(keyword,test,ifExpr,elseExpr) {
-      return isTrue(test) ? eval(ifExpr) : eval(elseExpr);
+      return isTrue(eval(test)) ? eval(ifExpr) : eval(elseExpr);
     })
   }
 };
 
 
 var number = {
+  id: "number",
   is: function(x) {
     return typeof x == "number"
   },
@@ -53,6 +74,7 @@ var number = {
   }
 };
 var string = {
+  id: "string",
   is: function(x) {
     return typeof x == "string"
   },
@@ -60,32 +82,94 @@ var string = {
     return x
   }
 };
-var call = {
+var bool = {
+  id: "boolean",
   is: function(x) {
-    return isList(x);
+    return typeof x == "boolean"
+  },
+  eval: function(x) {
+    return x
+  }
+};
+var call = {
+  id: "call",
+  is: function(x) {
+    return tap(isList(x) && (isSymbol(first(x)) || tagged("lambda",first(x))),function(was) {
+      //console.log(was,x)
+    })
   },
   eval: function(x) {
     var fnSymbol = first(x);
-    var args = rest(x).map(eval)
-    var fn = get(env,fnSymbol);
+    var args = rest(x);
+
+    if(tagged("lambda",fnSymbol)) return applyLambda(fnSymbol,args);
+
+    var fn = get(fnSymbol);
+    if(!fn) symbolNotFound(fnSymbol)
+
+    if(isNative(fn)) return fn.apply(null,args.map(eval));
+    if(tagged("lambda",fn)) return applyLambda(fn,args);
+    cantCall(fn);
+  }
+};
+
+function cantCall(fn) {
+  throw new Error("Can't call '" + fn + "'");
+}
+
+var defn = {
+  id: "defn",
+  is: function(x) {
+    return tagged("defn",x);
+  },
+  eval: function(defn) {
+    return spread(defn,function(sym,name,args,body) {
+      set(name,["lambda",args,body])
+    })
+  }
+};
+var begin = {
+  id: "begin",
+  is: isList,
+  eval: function(xs) {
+    if(xs.length == 0) return;
+    var last = xs.pop();
+    xs.forEach(eval);
+    return eval(last);
   }
 }
+
+function tagged(tag,list) {
+  return isList(list) && first(list) == tag;
+}
+
+function symbolNotFound(symbol) {
+  throw new Error("Could not find symbol '" + symbol + "'");
+}
+
+
+function isNative(x) {
+  return typeof x == "function"
+}
+
+function applyLambda(lambda,args) {
+  return spread(lambda,function(tag,params,body) {
+    if(args.length != params.length) {
+      throw new Error("Args mismatch, expected " + params.length + " got " + args.length);
+    }
+    var argVals = toMap(zip(params,args.map(eval)));
+    var toEval = replaceArguments(body,argVals);
+    return begin.eval(toEval);
+  })
+};
 
 function replaceArguments(body,args) {
   var replaceArg = function(x) {
-    if(isSymbol(x) && args[x.slice(1)] != null) return args[x.slice(1)];
-    if(isList(x)) return replaceArgs(x);
+    if(isSymbol(x) && args[x] != null) return args[x];
+    if(isList(x)) return x.map(replaceArg);
     return x;
   }
-  var replaceArgs = function(expression) {
-    return expression.map(replaceArg);
-  }
-  return replaceArgs(body,args);
-}
-
-function apply(proc,args) {
-  var body = replaceArguments(proc,args);
-  return eval(body);
+  return replaceArg(body,args);
 }
 
 function undefinedFunction(name) {
@@ -101,22 +185,40 @@ function binaryOperation(op,a,b) {
   undefinedFunction(op);
 }
 
+function set(key,val) {
+  return env[":" + key] = val;
+}
+function get(key) {
+  return env[":" + key];
+}
+
+
 function baseEnvironment() {
-  // TODO - make binary ops special forms, or can they be real fns?
-  var env = makeEnvironment();
-  set(env,">",function(a,b) { a > b; });
-  set(env,"<",function(a,b) { a < b; });
-  set(env,">=",function(a,b) { a >= b; });
-  set(env,"<=",function(a,b) { a <= b; });
-  set(env,"=",function(a,b) { a === b; });
+  set(">",function(a,b) { return a > b; });
+  set("<",function(a,b) { return a < b; });
+  set(">=",function(a,b) { return a >= b; });
+  set("<=",function(a,b) { return a <= b; });
+  set("=",function(a,b) { return a === b; });
+  set("+",function(a,b) { return a + b; });
+  set("*",function(a,b) { return a * b; });
+  set("/",function(a,b) { return a / b; });
+  set("-",function(a,b) { return a - b; });
+  set("%",function(a,b) { return a % b; });
+  set("print",function(a) { return console.log(a) });
 }
 
 var forms = [
   anIf,
   number,
-  string
+  string,
+  bool,
+  defn,
+  call,
+  begin
 ]
 
+env = {}
+baseEnvironment()
 
 module.exports = eval = function(expression) {
   var evaluator = find(forms,function(form) {
@@ -127,4 +229,3 @@ module.exports = eval = function(expression) {
 }
 
 
-eval.apply = apply;
--- test.js
+++ test.js
@@ -1,17 +1,62 @@
 var eval = require("./interpreter.js");
 var assert = require("chai").assert;
 
+
+
 describe("interpreter",function() {
   it("can evaluate ifs and primitives",function() {
-    var program1 = ["if",[">",2,2],"hello","fooby"]
+    var program1 = [["if",[">",2,2],"hello","fooby"]];
     var result = eval(program1);
     assert.equal(result,"fooby");
   })
 
-  context("apply",function() {
-    it("applies args to fn body and evaluates",function() {
-      var result = eval.apply(["if",true,":a",":b"],{a: "yay",b: "boo"});
+  context("primitives",function() {
+    it("if",function() {
+      var result = eval([["if",true,"yay","boo"]]);
       assert.equal(result,"yay");
     })
+  })
+
+
+  context("lambda",function() {
+    var program = [
+      [["lambda",["x"],[["*","x","x"]]],9]
+    ];
+
+    it("can run a lambda",function() {
+      var result = eval(program);
+      assert.equal(result,81);
+    })
+
+  });
+
+  context("defining fns",function() {
+
+    var program = [
+      ["defn","square",["x"],[["*","x","x"]]],
+      ["square",9]
+    ];
+
+    it("can run a defined fn",function() {
+      var result = eval(program);
+      assert.equal(result,81);
+    })
+
+  })
+
+
+  context("defining fns",function() {
+
+    var program = [
+      ["defn","square",["x"],[["*","x","x"]]],
+      ["if",[">",["square",9],80],
+        ["print","yay!"],
+        ["print","arithmetic broken"]]
+    ];
+
+    it("runs",function() {
+      eval(program);
+    })
+
   })
 })
